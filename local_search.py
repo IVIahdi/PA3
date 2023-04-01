a6ab21b985fc7ae17906ad67d8231d8d2527ea34
IVIahdi <mahdi.11231@gmail.com>
2023-04-01 08:08:40 +0300

    KS

--- local_search.py
+++ local_search.py
@@ -4,19 +4,11 @@ import copy
 def tour_cost(state, adj_matrix):
     cost = 0
     for i in range(len(state)):
-        j = (i + 1) % len(state)
-        if not np.isnan(adj_matrix[state[i], state[j]]):
-            if state[i] < state[j]:
-                cost += adj_matrix[state[i], state[j]]
-        else:
-            return np.nan
-    if state[0] != state[-1]:
-        return np.nan
-    visited = set()
-    for loc in state:
-        if loc in visited:
-            return np.nan
-        visited.add(loc)
+        for j in range(i):
+            if adj_matrix[state[i],state[j]] != np.nan:
+                cost += adj_matrix[state[i],state[j]]
+            else:
+                return np.nan
     return cost
 
 def random_swap(state):
@@ -25,19 +17,23 @@ def random_swap(state):
     new_state[idx1], new_state[idx2] = new_state[idx2], new_state[idx1]
     return new_state
 
+def schedule(t):
+    return t*0.99
 def simulated_annealing(initial_state, adj_matrix, initial_T=1000):
     T = initial_T
     current = initial_state
     iters = 0
-    while T >= 1e-14:
-        T *= 0.99
-        if T < 1e-14:
+    while True:
+        T = schedule(T)
+        if T < 10e-14:
             break
         next_state = random_swap(current)
-        deltaE = tour_cost(next_state, adj_matrix) - tour_cost(current, adj_matrix)
+        deltaE = tour_cost(current, adj_matrix) - tour_cost(next_state, adj_matrix)
+        u = np.random.uniform()
         if deltaE > 0:
             current = next_state
-        elif np.exp(deltaE / T) > np.random.uniform():
+        elif deltaE <= 0 and u <= np.exp(deltaE / T):
             current = next_state
+        
         iters += 1
     return current, iters
